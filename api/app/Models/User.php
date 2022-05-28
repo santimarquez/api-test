830040d90e086daab04e7e2a7846895fae67ce26
Santi Márquez <santiago.marquez@alertacoches.es>
2022-05-28 18:02:00 +0200

    Ignored the timestamps on a user creation

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -26,6 +26,8 @@ class User extends Authenticatable
         'update_time'
     ];
 
+    public $timestamps = false;
+
     /**
      * The attributes that should be hidden for serialization.
      *
